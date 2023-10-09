738cf493d5faf532a5d18248ea2cc38720fc4f6d
Gyan Lima <gyan.patrick1@gmail.com>
2023-10-09 12:02:15 -0300

    colorindo as bolinhas conforme os slide muda

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 var slideItem = 0;
+var bolinhas = document.getElementsByClassName('bolinha');
+
 window.onload = function() {
 	setInterval(passarSlide, 2000);
 
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	var objs = document.getElementsByClassName("slide");
+	bolinhas.item(slideItem).style.backgroundColor = "red";
 	for(var i=0;i<objs.length;i++) {
 		objs[i].style.width = slidewidth+"px";
 	}
@@ -12,9 +15,13 @@ function passarSlide() {
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	
 	if(slideItem >= 3) {
+		bolinhas.item(slideItem).style.backgroundColor = "#ccc";
 		slideItem = 0;
+		bolinhas.item(slideItem).style.backgroundColor = "red";
 	} else {
 		slideItem++;
+		bolinhas.item(slideItem).style.backgroundColor = "red";
+		bolinhas.item(slideItem-1).style.backgroundColor = "#ccc";
 	}
 
 	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
